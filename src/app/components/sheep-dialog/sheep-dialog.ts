35fc6ba7eaf8e20bd288d0734ab00d45fe62e8c8
Loic Magnette <loic.magnette@gmail.com>
2025-10-20 14:14:38 +0200

    #12 [SIGNAL] forms

--- src/app/components/sheep-dialog/sheep-dialog.ts
+++ src/app/components/sheep-dialog/sheep-dialog.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle} from '@angular/material/dialog';
 import {MatError, MatFormField, MatInput, MatLabel} from '@angular/material/input';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButton} from '@angular/material/button';
 import {Sheep} from '../../models/sheep';
+import {form, Field} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-sheep-dialog',
@@ -17,16 +18,17 @@ import {Sheep} from '../../models/sheep';
     MatFormField,
     MatButton,
     MatDialogTitle,
-    MatError
+    MatError,
+    Field
   ],
   template:`
     <h2 mat-dialog-title>Add a New Space Sheep</h2>
     <mat-dialog-content>
-      <form [formGroup]="sheepForm" class="sheep-form">
+      <form class="sheep-form">
         <mat-form-field appearance="outline">
           <mat-label>Sheep Name</mat-label>
-          <input matInput formControlName="name" placeholder="Enter sheep name">
-          @if (sheepForm.get('name')?.invalid) {
+          <input matInput [field]="sheepForm.name" placeholder="Enter sheep name">
+          @if (sheepForm.name().invalid()) {
             <mat-error>
               Name is required and must be at least 2 characters
             </mat-error>
@@ -35,8 +37,8 @@ import {Sheep} from '../../models/sheep';
 
         <mat-form-field appearance="outline">
           <mat-label>Description</mat-label>
-          <textarea matInput formControlName="description" placeholder="Describe this cosmic sheep" rows="4"></textarea>
-          @if (sheepForm.get('description')?.invalid) {
+          <textarea matInput [field]="sheepForm.description" placeholder="Describe this cosmic sheep" rows="4"></textarea>
+          @if (sheepForm.description().invalid()) {
             <mat-error>
               Description is required and must be at least 10 characters
             </mat-error>
@@ -47,32 +49,34 @@ import {Sheep} from '../../models/sheep';
     </mat-dialog-content>
     <mat-dialog-actions align="end">
       <button mat-button (click)="onCancel()">Cancel</button>
-      <button mat-raised-button color="accent" [disabled]="sheepForm.invalid" (click)="onSubmit()">Add Sheep</button>
+      <button mat-raised-button color="accent" [disabled]="sheepForm().invalid()" (click)="onSubmit()">Add Sheep</button>
     </mat-dialog-actions>
   `,
   styleUrl: './sheep-dialog.scss'
 })
 export class SheepDialog {
 
-  sheepForm: FormGroup;
+  sheep  = signal<Sheep>({
+    id:9999,
+    name:'',
+    description:'',
+    imageUrl:'assets/images/unknown.webp',
+    species:'sheep',
+    likes:0,
+    category:'unknown'
+  })
+
+  sheepForm= form(this.sheep)
 
   constructor(
-    private fb: FormBuilder,
     private dialogRef: MatDialogRef<SheepDialog>
   ) {
-    this.sheepForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(2)]],
-      description: ['', [Validators.required, Validators.minLength(10)]],
-      imageUrl: ['assets/images/unknown.webp'],
-      species: ['sheep'],
-      likes: [0],
-      category: ['unknown']
-    });
+
   }
 
   onSubmit(): void {
-    if (this.sheepForm.valid) {
-      const newSheep: Partial<Sheep> = this.sheepForm.value;
+    if (this.sheepForm().valid()) {
+      const newSheep: Partial<Sheep> = this.sheepForm().value();
       this.dialogRef.close(newSheep);
     }
   }
