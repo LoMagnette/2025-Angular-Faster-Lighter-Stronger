5297536b346d9e74500ceeba9ed35a1885b94449
Loic Magnette <loic.magnette@gmail.com>
2025-11-07 08:58:17 +0100

    remove unnecessary provider since it's by default since angular 21

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,6 +12,5 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient()
   ]
 };
